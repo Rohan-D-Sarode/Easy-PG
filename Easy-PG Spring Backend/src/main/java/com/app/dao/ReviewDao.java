8d7553583b1d8461d32a71c356caa6a870a79933
ROHAN SARODE <rohan.sarode.cdac@gmail.com>
2023-08-30 13:29:39 +0530

    delete

--- Easy-PG Spring Backend/src/main/java/com/app/dao/ReviewDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.Reviews;
-
-public interface ReviewDao extends JpaRepository<Reviews, Long>{
-
-}
